bb2ac564c991ced5b97492c2d4386acbd9e6db7c
siskawatiR <siskawatirahayu95@gmail.com>
2020-04-26 20:11:49 +0700

    tambahan daftar,masuk,awal

--- app/src/main/java/com/dofazer/cafe/app/main/fragment/BerandaFragment.kt
+++ app/src/main/java/com/dofazer/cafe/app/main/fragment/BerandaFragment.kt
@@ -7,13 +7,16 @@ import android.view.View
 import android.view.ViewGroup
 import com.dofazer.cafe.R
 
-class TutorFragment : Fragment() {
+
+class BerandaFragment : Fragment() {
+
+
     companion object{
-        fun newInstance() = TutorFragment()
+        fun newInstance() = BerandaFragment()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_tutor, container, false)
+        return inflater.inflate(R.layout.fragment_beranda, container, false)
 
     }
 
